9bd2966c709cea45706cc7ff47e9c7ac907566b5
tdhris <tdhristova@yahoo.com>
2014-03-20 12:09:19 +0200

    updating list of commands with new commands

--- mail.py
+++ mail.py
@@ -1,5 +1,5 @@
 import List
-from assist_functions import greet_user, print_list_of_commands, get_list_name_from_command, add_person_to_list_file, get_arguments, merge_lists, export
+import assist_functions
 
 def process_input(command):
 	if command == 'exit':
@@ -15,12 +15,12 @@ def process_input(command):
 		pass
 
 	elif command.startswith('add '):
-		list_name = get_list_name_from_command(command)
+		list_name = assist_functions.get_list_name_from_command(command)
 		new_person = add_person()
 		add_person_to_list_file(new_person, list_name)
 
 	elif command.startswith('create '):
-		list_name = get_arguments(command, 1)
+		list_name = assist_functions.get_arguments(command, 1)
 		print("{} was created".format(list_name))
 		new_list = List.List(list_name)
 
@@ -28,14 +28,19 @@ def process_input(command):
 		pass
 
 	elif command.startswith('merge_lists'):
-		arguments = get_arguments(command, 3)
+		arguments = assist_functions.get_arguments(command, 3)
 		first_list, second_list, name_new_list = arguments[0], arguments[1], arguments[2]
 		merge_lists(first_list, second_list, name_new_list)
 
 	elif command.startswith('export'):
-		list_index = get_arguments(command, 1)
-		export(list_index)
+		list_index = assist_functions.get_arguments(command, 1)
+		assist_functions.export(list_index)
 		
+update_subscriber <unique_list_identifier> <unique_name_identifier>
+* remove_subscriber <unique_list_identifier> <unique_name_identifier>
+ update <unique_list_identifier>  <new_list_name>
+ "import <filename"
+
 	else:
 		pass
 
@@ -51,7 +56,7 @@ def add_person():
 	return new_person
 
 def main():
-	greet_user()
+	assist_functions.greet_user()
 	command = take_input()
 
 	while True:
