7c7f4c608c5f505e1f4440ce962d99f99f592936
Boiannn <bdd@mail.bg>
2014-03-20 12:10:44 +0200

    almost no changes at all :((

--- List.py
+++ List.py
@@ -1,13 +1,46 @@
 import json
+import sys
 
 class Person:
 	def __init__(self, name, email):
 		self.name = name
 		self.email = email
 
+	def search_email(self, email):
+		
 
 class List:
-	def __init__(self, list_name):
+	def __init__(self, list_name, identifier):
 		self.list_name = list_name
+		self.identifier = identifier
 		self.create_list_file()
 		self.add_list_to_archive()
+
+	def show_lists(self):
+		
+		def read(self):
+			if(sys.argv) > 1:
+				file = open("lists.txt", "r")
+				print(file.read())
+			else:
+				print "No lists!"
+		
+		for arg in range(1,len(sys.argv)):
+		read(sys.argv[arg])
+
+		
+
+	def show_list(self,identifier):
+		self.identifier = sys.argv
+
+		def read(self):
+			if(sys.argv) > 1:
+				file = open("lists.txt", "r")
+				print(file.read())
+			else:
+				print "No lists!"
+		
+		for arg in range(1,len(sys.argv)):
+		read(sys.argv[arg])
+
+
